f568e738266072ce0cb9f7cb0d3b3f97cce4cec6
Sener Topaloglu <psyst6@exmail.nottingham.ac.uk>
2024-03-18 11:22:31 +0000

    add json implementation

--- json/src/main/scala/Json.scala
+++ json/src/main/scala/Json.scala
@@ -1,1 +1,61 @@
 package com.jpmc.json
+
+sealed abstract class Json extends Product with Serializable
+
+object Json {
+  case class Object(data: Map[java.lang.String, Json]) extends Json
+
+  case class Array(data: List[Json]) extends Json
+
+  case class String(value: java.lang.String) extends Json
+
+  case class Number(value: BigDecimal) extends Json
+
+  case class Boolean(value: scala.Boolean) extends Json
+
+  case object Null extends Json
+
+  def apply(value: java.lang.String): Json = String(value)
+
+  def apply(value: BigDecimal): Json = Number(value)
+
+  def apply(value: scala.Boolean): Json = Boolean(value)
+
+  def apply(value: List[Json]): Json = Array(value)
+
+  def apply(value: Map[java.lang.String, Json]): Json = Object(value)
+
+  def prettyPrintVal(input: Json): java.lang.String = {
+    input match {
+      case String(value) => s"""\"$value\""""
+      case Number(value) => value.toString
+      case Boolean(value) => value.toString
+      case Null => "null"
+      case Array(values) => prettyPrintDocument(Array(values))
+      case Object(values) => prettyPrintDocument(simplifyNull(Object(values)))
+    }
+  }
+
+  def prettyPrintDocument(document: Json): java.lang.String = {
+    document match {
+      case Array(values) => values.map(prettyPrintVal).mkString("[", ",", "]")
+      case Object(pairs) => pairs.map {
+        case (key, value) => s""""$key":${prettyPrintVal(value)}"""
+      }.mkString("{", ",", "}")
+      case _ => prettyPrintVal(document)
+    }
+  }
+
+  def simplifyNull(input: Object): Object = {
+    //input.data.filter((name, value) => value != null)
+    Object(input.data.filter(_._2 != Null))
+  }
+
+
+  def main(args: scala.Array[java.lang.String]): Unit = {
+    println(prettyPrintVal(simplifyNull(Object(Map[java.lang.String, Json]("name" -> String("John Doe"), "age" -> Null)))))
+  }
+
+
+
+}
--- json/src/test/scala/JsonTests.scala
+++ json/src/test/scala/JsonTests.scala
@@ -3,11 +3,41 @@ package com.jpmc.json
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import com.jpmc.json.Json
 
 class JsonTests extends AnyFunSuite with ScalaCheckPropertyChecks with Matchers {
-  test("Some test") {
-    forAll { str: String =>
-      str should be(str)
-    }
+
+  import TestHelper._
+
+  test("prettyPrint Json Array") {
+    val result = Json.prettyPrintDocument(exampleArray)
+    val expected =
+      """["Apples",true]"""
+    result should be(expected)
   }
+
+  test("prettyPrint Json Object") {
+    val result = Json.prettyPrintDocument(exampleObject)
+    val expected =
+      """{"name":"John Doe","age":30,"profession":"Taxi Driver"}"""
+    result should be(expected)
+  }
+}
+
+object TestHelper {
+  val exampleArray: Json =
+    Json(
+      List(
+        Json.String("Apples"), Json.Boolean(true)
+      )
+    )
+
+  val exampleObject: Json =
+    Json(
+      Map(
+        ("name", Json.String("John Doe")),
+        ("age", Json.Number(30)),
+        ("profession", Json.String("Taxi Driver"))
+      )
+    )
 }
