9fdaa09174ffa05cae5d4c0f3eb5938f1c069432
Sener Topaloglu <psyst6@exmail.nottingham.ac.uk>
2024-03-18 11:22:03 +0000

    add encoder implementation

--- codecs/src/main/scala/Encoder.scala
+++ codecs/src/main/scala/Encoder.scala
@@ -1,1 +1,76 @@
 package com.jpmc.codecs
+
+import com.jpmc.json.Json
+import com.jpmc.codecs.User
+
+import java.time.LocalDate
+
+trait Encoder[A] {
+  def encode(value: A): Json
+}
+
+object Encoder {
+
+  def apply[A](implicit encoder: Encoder[A]): Encoder[A] = encoder
+
+  def from[A](f: A=>Json):Encoder[A] =
+    new Encoder[A] {
+      override def encode(value: A): Json = f(value)
+    }
+
+  def encode[A](input: A)(implicit encoder: Encoder[A]): Json = encoder.encode(input)
+
+  implicit class EncoderOps[A](a: A) {
+    def asJson(implicit encoder: Encoder[A]): Json = encoder.encode(a)
+  }
+
+  implicit val booleanEncoder: Encoder[Boolean] = Json.Boolean(_)
+
+  implicit val stringEncoder: Encoder[java.lang.String] = Json.String(_)
+
+  implicit val intEncoder: Encoder[Int] = Json.Number(_)
+
+  implicit val dateEncoder: Encoder[LocalDate] = date => Json.String(date.toString)
+
+  implicit def tuple2Encoder[A,B](implicit encoder1: Encoder[A], encoder2: Encoder[B]): Encoder[Tuple2[A,B]] = t2 =>
+    Json.Object(Map("_1" -> encoder1.encode(t2._1), "_2" -> encoder2.encode(t2._2)))
+
+  implicit def tuple3Encoder[A, B, C](implicit e1: Encoder[A], e2: Encoder[B], e3: Encoder[C]): Encoder[Tuple3[A, B, C]] = t3 =>
+    Json.Object(Map("_1" -> e1.encode(t3._1), "_2" -> e2.encode(t3._2), "_3" -> e3.encode(t3._3)))
+
+  implicit def listEncoder[A](implicit encoder: Encoder[A]): Encoder[List[A]] = values =>
+    Json.Array(values.map(encoder.encode))
+
+  implicit def vectorEncoder[A](implicit encoder: Encoder[A]): Encoder[Vector[A]] = values =>
+    Json.Array(values.map(encoder.encode).toList)
+
+  implicit def optionEncoder[A](implicit encoder: Encoder[A]): Encoder[Option[A]] = value =>
+    value match {
+      case Some(x) => encoder.encode(x)
+      case None => Json.Null
+    }
+
+  implicit def eitherEncoder[A, B](implicit leftEncoder: Encoder[A], rightEncoder: Encoder[B]): Encoder[Either[A, B]] = value =>
+    value match {
+      case Left(value) => leftEncoder.encode(value)
+      case Right(value) => rightEncoder.encode(value)
+    }
+
+  implicit val kindEncoder: Encoder[User.Kind] = from {
+    case User.Kind.Normal => Json.String("Normal")
+    case User.Kind.Privileged => Json.String("Privileged")
+    case User.Kind.Guest => Json.String("Guest")
+  }
+
+  implicit val userEncoder: Encoder[User] =
+    from {
+      user => Json.Object(
+        Map(
+          "name" -> user.name.asJson,
+          "age" -> user.age.asJson,
+          "kind" -> user.kind.asJson
+        )
+      )
+    }
+
+}
--- codecs/src/test/scala/EncoderTests.scala
+++ codecs/src/test/scala/EncoderTests.scala
@@ -3,11 +3,115 @@ package com.jpmc.codecs
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import com.jpmc.json.Json
+
+import java.time.LocalDate
 
 class EncoderTests extends AnyFunSuite with ScalaCheckPropertyChecks with Matchers {
-  test("Some test") {
-    forAll { str: String =>
-      str should be(str)
-    }
+  test("Boolean Encoder") {
+    val booleanTrue = true
+    val result = Encoder.encode(booleanTrue)
+    val expected = Json.Boolean(true)
+    result should be(expected)
   }
+
+  test("String Encoder") {
+    val stringExample = "John Doe"
+    val result = Encoder.encode(stringExample)
+    val expected = Json.String("John Doe")
+    result should be(expected)
+  }
+
+  test("Integer Encoder") {
+    val numberExample = 10
+    val result = Encoder.encode(numberExample)
+    val expected = Json.Number(10)
+    result should be(expected)
+  }
+
+  test("Date Encoder") {
+    val dateExample = LocalDate.of(2012, 10, 1)
+    val result = Encoder.encode(dateExample)
+    val expected = Json.String("2012-10-01")
+    result should be(expected)
+  }
+
+  test("Tuple2 Encoder") {
+    val example: Tuple2[String, Int] = ("John Doe", 100)
+    val result = Encoder.encode(example)
+    val expected =
+      Json.Object(
+        Map(
+          "_1" -> Json.String("John Doe"),
+          "_2" -> Json.Number(100)
+        )
+      )
+    result should be(expected)
+  }
+
+  test("Tuple3 Encoder") {
+    val example: Tuple3[String, Int, Boolean] = ("John Doe", 100, false)
+    val result = Encoder.encode(example)
+    val expected =
+      Json.Object(
+        Map(
+          "_1" -> Json.String("John Doe"),
+          "_2" -> Json.Number(100),
+          "_3" -> Json.Boolean(false)
+        )
+      )
+    result should be(expected)
+  }
+
+  test("List Encoder") {
+    val example = List("A", "B", "C", "D")
+    val result = Encoder.encode(example)
+    val expected = Json.Array(List(Json.String("A"), Json.String("B"), Json.String("C"),Json.String("D")))
+    result should be(expected)
+  }
+
+  test("Vector Encoder") {
+    val example = Vector("A", "B", "C", "D")
+    val result = Encoder.encode(example)
+    val expected = Json.Array(List(Json.String("A"), Json.String("B"), Json.String("C"), Json.String("D")))
+    result should be(expected)
+  }
+
+  test("Option Encoder - Some") {
+    val example: Option[Int] = Some(10)
+    val result = Encoder.encode(example)
+    val expected = Json.Number(10)
+    result should be(expected)
+  }
+
+  test("Option Encoder - None") {
+    val example: Option[Int] = None
+    val result = Encoder.encode(example)
+    val expected = Json.Null
+    result should be(expected)
+  }
+
+  test("Either Encoder - Int, Int") {
+    val example: Either[Int, Int] = Left(4)
+    val result = Encoder.encode(example)
+    val expected = Json.Number(4)
+    result should be(expected)
+  }
+
+  test("User Encoder") {
+    val example: User = User("john doe", 21, User.Kind.Normal)
+    val result = Encoder.encode(example)
+    val expected = {
+      Json.Object(
+        Map(
+          "name" -> Json.String("john doe"),
+          "age" -> Json.Number(21),
+          "kind" -> Json.String("Normal")
+        )
+      )
+    }
+    result should be(expected)
+  }
+
+
 }
